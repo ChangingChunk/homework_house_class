e2d4daba633fe0a2f1aac68d0c351a5e262ef427
andrewsimpson90 <33570851-andrewsimpson90@users.noreply.replit.com>
2024-06-13 15:02:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+class House:
+  def __init__(self, name, number_of_floors):
+    self.name = name
+    self.number_of_floors = number_of_floors
+
+  def go_to(self, new_floor):
+    if new_floor <= self.number_of_floors and new_floor > 0:
+      i =1
+      while i <= int(new_floor):
+        print(i)
+        i += 1
+    else:
+      print(f'Этажа {new_floor} не существует')
+
+elbrus = House('ЖК эльбрус', 10)
+dream = House('ЖК мечта', 30)
+elbrus.go_to(5)
+elbrus.go_to(11)
+elbrus.go_to(-7) 
+dream.go_to(15)
+dream.go_to(70)
